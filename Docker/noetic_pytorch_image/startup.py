4822272af67158db2c07b60e2424f6f622040a03
HuguesTHOMAS <huguesthomas218@gmail.com>
2021-09-09 14:55:03 +0000

    .

--- Docker/noetic_pytorch_image/startup.py
+++
@@ -1,9 +0,0 @@
-print("Running .startup.py")
-import sys
-import os
-user = os.environ['USER']
-sys.path.insert(0, "/home/" + user + "/catkin_ws/")
-from src.dashboard import *
-print("imported dashboard")
-d = Dashboard()
-print("d = Dashboard()")
