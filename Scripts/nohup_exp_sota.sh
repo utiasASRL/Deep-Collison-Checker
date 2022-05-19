903f03843adb36927dd9a9b2faf84fb776219467
HuguesTHOMAS <huguesthomas218@gmail.com>
2022-05-19 22:26:26 +0000

    .

--- Scripts/nohup_exp_sota.sh
+++ Scripts/nohup_exp_sota.sh
@@ -2,4 +2,6 @@
 
 now=`date +%Y-%m-%d_%H-%M-%S`
 NOHUP_FILE="$PWD/../Data/Simulation_v2/nohup_logs/log-exp_$now.txt"
+PID_FILE="$PWD/../Data/Simulation_v2/nohup_logs/pid-exp_$now.txt"
 nohup ./exp_sota.sh > "$NOHUP_FILE" 2>&1 &
+echo $! > "$PID_FILE"
